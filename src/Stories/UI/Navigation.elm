1be647344a0a27098e2d7d3b9a959c32bab9db4b
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2023-01-05 15:56:08 +0100

    Add Navigation

---
+++ src/Stories/UI/Navigation.elm
@@ -0,0 +1,82 @@
+module Stories.UI.Navigation exposing (..)
+
+import Array
+import Helper exposing (col)
+import Html exposing (Html)
+import Storybook.Story exposing (Story)
+import UI.Click as C
+import UI.Icon as I
+import UI.Navigation as N
+import UI.Nudge
+import UI.Tooltip
+
+
+main : Story () Msg
+main =
+    Storybook.Story.stateless
+        { view = view
+        }
+
+
+type Msg
+    = UserClicked
+
+
+sampleNavItem : N.NavItem Msg
+sampleNavItem =
+    N.navItem "some" (C.ExternalHref "some")
+
+
+sampleNavItems : Array.Array (N.NavItem Msg)
+sampleNavItems =
+    [ C.ExternalHref "link 1" |> N.navItem "item 1"
+    , C.ExternalHref "link 2" |> N.navItem "item 2"
+    , C.ExternalHref "link 3" |> N.navItem "item 3"
+    , C.ExternalHref "link 4" |> N.navItem "item 4"
+    ]
+        |> Array.fromList
+
+
+elements : List (N.Navigation Msg)
+elements =
+    [ N.withNoSelectedItems
+        (sampleNavItems
+            |> Array.toList
+        )
+        N.empty
+    , N.withItems
+        (Array.slice 0 2 sampleNavItems
+            |> Array.toList
+        )
+        (Array.get 2 sampleNavItems
+            |> Maybe.withDefault sampleNavItem
+        )
+        (Array.slice 3 4 sampleNavItems
+            |> Array.toList
+        )
+        N.empty
+    , N.withNoSelectedItems
+        (sampleNavItems
+            |> Array.toList
+            |> List.map (N.navItemWithIcon I.unisonMark)
+        )
+        N.empty
+    , N.withNoSelectedItems
+        (sampleNavItems
+            |> Array.toList
+            |> List.map (N.navItemWithNudge (UI.Nudge.nudge |> UI.Nudge.pulsate))
+        )
+        N.empty
+    , N.withNoSelectedItems
+        (sampleNavItems
+            |> Array.toList
+            |> List.map (N.navItemWithTooltip (UI.Tooltip.text "tooltip text" |> UI.Tooltip.tooltip))
+        )
+        N.empty
+    ]
+
+
+view : Html Msg
+view =
+    (elements |> List.map N.view)
+        |> col []
